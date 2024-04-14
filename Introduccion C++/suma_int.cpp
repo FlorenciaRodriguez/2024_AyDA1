bbac0a7a3cdef023410012f1ac059e82324ea661
Florencia <rodrig.florencia@gmail.com>
2024-04-13 21:45:13 -0300

    Create suma_int.cpp

---
+++ Introduccion C++/suma_int.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int num1,num2;
+    cout << "Ingrese un numero y luego otro numero \n";
+    cin >> num1 >> num2;
+    cout << "La suma es: "<< num1 + num2 << endl;
+    return 0;
+}
