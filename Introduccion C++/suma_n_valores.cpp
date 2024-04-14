8958d6669566fad27050ff87639032da391f0a08
Florencia <rodrig.florencia@gmail.com>
2024-04-13 21:46:43 -0300

    Create suma_n_valores.cpp

---
+++ Introduccion C++/suma_n_valores.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int numero = 0;
+    int suma = 0;
+    cout << "Ingrese numeros (ingrese 0 para terminar):\n";
+    do {
+        cout << "Ingrese un numero: ";
+        cin >> numero;
+        if (numero != 0) {
+            suma += numero;
+        }
+    } while (numero != 0);
+    cout << "La suma de los numeros ingresados es: " << suma << endl;
+    return 0;
+}
