734e543a8da111f627038d73f61a61cd3285dcfa
Florencia <rodrig.florencia@gmail.com>
2024-04-13 21:58:55 -0300

    Update cmath_factorial.cpp

--- Introduccion C++/cmath_factorial.cpp
+++ Introduccion C++/cmath_factorial.cpp
@@ -2,14 +2,16 @@
 #include <cmath> // La biblioteca cmath proporciona diversas funciones matematicas, como pow, sqrt, abs, entre otras
 using namespace std;
 
-int main() {
+int main() 
+{
     int nro;
     cout << "Ingrese un numero para calcular su factorial: ";
     cin >> nro;
     nro = abs(nro);
 
     unsigned long resultado = 1;
-    for (unsigned int i = 2; i <= nro; i++) {
+    for (unsigned int i = 2; i <= nro; i++) 
+    {
         resultado *= i;
     }
     cout << "El factorial de " << nro << " es: " << resultado << endl;
