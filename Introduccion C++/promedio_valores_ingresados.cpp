82ff0c7115cda927d2dab127d810c44cef42add2
Florencia <rodrig.florencia@gmail.com>
2024-04-13 21:46:11 -0300

    Create promedio_valores_ingresados.cpp

---
+++ Introduccion C++/promedio_valores_ingresados.cpp
@@ -0,0 +1,9 @@
+int main()
+{
+    float num1, num2, promedio;
+    cout << "Ingrese un numero y luego otro numero \n";
+    cin >> num1 >> num2;
+    promedio = (num1 + num2) / 2;
+    cout << "El promedio es: "<< promedio << endl;
+    return 0;
+}
