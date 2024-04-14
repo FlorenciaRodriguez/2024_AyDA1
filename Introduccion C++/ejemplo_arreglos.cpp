41f44fbb6dd8084acea96efaee3cbfb0033e6963
Florencia <rodrig.florencia@gmail.com>
2024-04-13 21:54:15 -0300

    Update ejemplo_arreglos.cpp

--- Introduccion C++/ejemplo_arreglos.cpp
+++ Introduccion C++/ejemplo_arreglos.cpp
@@ -5,7 +5,13 @@ int main() {
     const int TAMANO = 5;
     char arreglo[TAMANO];
     cout << "Ingrese " << TAMANO << " char para llenar el arreglo:\n";
-
+    
+    // Alternativa: pedir el tamaño del arreglo por consola y declarar el arreglo.
+    // int tamanio;
+    // cout<< "Ingrese el tamaño del arreglo:\n";
+    // cin >> tamanio;
+    // char arreglo[tamanio];
+    
     for (int i = 0; i < TAMANO; i++) {
         cout << "Ingrese el elemento " << i + 1 << ": ";
         cin >> arreglo[i];
