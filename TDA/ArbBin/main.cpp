5e56825def53de9b485685d00689f811052896f2
Florencia <rodrig.florencia@gmail.com>
2024-06-19 23:26:39 -0300

    Update main.cpp

--- TDA/ArbBin/main.cpp
+++ TDA/ArbBin/main.cpp
@@ -1,26 +1,24 @@
 #include <iostream>
 #include "Arbin.h"
-
-
 using namespace std;
 
-// void pre_orden( Arbin<int>* arbol){
-//     if (!arbol->es_vacio()) {
-//          cout << arbol->raiz()<< endl ;
-//          pre_orden(arbol->arbol_izquierdo());
-//          pre_orden(arbol->arbol_derecho());
-//     }
-// }
+std::ostream & operator << (std::ostream & salida, Arbin<int> & arbol)
+{
+    arbol.preOrden(salida);
+    return salida;
+}
 
-// std::ostream & operator << (std::ostream & salida, Arbin<int> & arbol)
-// {
-//   pre_orden (&arbol);
-//   return salida;
-// }
+template <typename T>
+T getMax(T x, T y)
+{
+    if (x>y)
+        return x;
+    return y;
+}
 
 int main()
 {
-    Arbin<int>* a1= new Arbin<int>();   /// vacios
+    Arbin<int>* a1= new Arbin<int>;
 	Arbin<int>* a2= new Arbin<int>;
 	Arbin<int>* a3= new Arbin<int>;
 	Arbin<int>* a11= new Arbin<int>;
@@ -34,17 +32,33 @@ int main()
 	a5->construir(a3,a4,90);
 	a6->construir(a5,a5,100);
 
-    Arbin<int>* a7 (a6); /// a7 se construye por copia
+	Arbin<int>* a7 (a6);
 
-                                                /// arbol =              17
-	Arbin<int>* arbol= new Arbin<int>;
-	Arbin<int>* a10= new Arbin<int>;             ///          100                 vacío
-	arbol->construir(a7, a10,17);               ///      90         90
+	cout << "Raiz de a3 (5): " << a3->raiz() <<endl;
+	cout << "Altura de a1 (0): " << a1->altura() <<endl;
+	cout << "Altura de a6 (3): " << a6->altura() <<endl;
+	cout << "Altura de a3 (1): " << a3->altura() <<endl;
+	cout << "# nodos de a6 (7): " << a6->cantidadNodos() <<endl;
+	cout << "# nodos de a5 (6): " << a5->cantidadNodos() <<endl;
+	cout << "True: " << a6->existeElemento(90) <<endl;
+	cout << "False: " << a6->existeElemento(190) <<endl;
+	cout << "True: " << a6->existeElemento(10) <<endl;
+	cout << "True: " << a6->existeElemento(100) <<endl;
+	cout << "True: " << a6->existeElemento(5) <<endl;
+	if (a6 == a7)
+        cout << "a6 y a7 son iguales" <<endl;
+	if (!a5->esVacio())
+        cout << "a5 no es vacio" << endl;
+    if (a1->esVacio())
+        cout << "a1 es vacio" << endl;
+    if (a3->esHoja())
+        cout << "a3 es hoja" << endl;
+    if (!a6->esHoja())
+        cout << "a6 no es hoja" << endl;
+    if (!(a5 == a7))
+        cout << "a5 y a7 no son iguales" <<endl;
 
-    // cout << "cantidad de nodos:"  ;         ///       5     10    5     10
-    // cout << arbol->cantidadNodos();
-    // cout <<  endl <<"elementos del arbol listados en preorden: " << endl ;
-    // cout << *arbol;
-
+    cout << "Recorrido pre orden del arbol a7: "<<endl;
+    cout << *a7;
     return 0;
 }
