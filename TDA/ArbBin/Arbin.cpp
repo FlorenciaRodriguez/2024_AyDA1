10e564802e25275d81ab70bad2c748c37f901f79
Florencia <rodrig.florencia@gmail.com>
2024-06-19 23:25:55 -0300

    Update Arbin.cpp

--- TDA/ArbBin/Arbin.cpp
+++ TDA/ArbBin/Arbin.cpp
@@ -1,128 +1,144 @@
 #include "Arbin.h"
 #include <cassert>
-#include <iostream>
 
-template <typename T>
-Arbin<T>::Arbin() {
-	inicio = NULL;
+template<typename T>
+Arbin<T>::Arbin()
+{
+    inicio = NULL;
 }
 
-template <typename T>
-Arbin<T>::~Arbin () {
-	vaciar (this);
+template<typename T>
+Arbin<T>::Arbin(const Arbin<T> & otro)
+{
+    this->operator=(otro);
 }
 
-template <typename T>
-void Arbin<T>::vaciar(Arbin<T>* a) {
-    if (a->inicio != NULL) {
-        vaciar(a->arbol_izquierdo());
-        vaciar(a->arbol_derecho());
+template<typename T>
+Arbin<T>::~Arbin()
+{
+    vaciar(this);
+}
 
+template<typename T>
+void Arbin<T>:: vaciar(Arbin<T> * a)
+{
+    if (a->inicio != NULL)
+    {
+        vaciar(a->arbolIzquierdo());
+        vaciar(a->arbolDerecho());
         delete a->inicio;
         a->inicio = NULL;
         delete a;
     }
 }
 
-template <typename T>
-Arbin<T>::Arbin(const Arbin<T>& otroArbol) {
-    inicio = NULL;
-    this->operator=(otroArbol);
-   }
-
-template <typename T>
-void Arbin<T>::construir(Arbin<T>* izq, Arbin<T>* der, const T & dato) {
-	Arbin* i = new Arbin(*izq);
-	Arbin* d = new Arbin(*der);
-	inicio =  new NodoArbol(dato,i,d);
+template<typename T>
+void Arbin<T>::construir( Arbin<T>* izq, Arbin<T>* der, const T & dato)
+{
+    Arbin<T> * i = new Arbin(*izq);
+    Arbin<T> * d = new Arbin(*der);
+    inicio = new NodoArbin(dato,i,d);
 }
 
 template <typename T>
 const T & Arbin<T>::raiz() const {
     assert (inicio);
-    return inicio->obtener_dato();
+    return inicio->getDato();
 }
 
 template <typename T>
-Arbin<T>* Arbin<T>::arbol_izquierdo() const {
+Arbin<T>* Arbin<T>::arbolIzquierdo() const {
     assert (inicio);
-    return inicio->obtener_izquierda();
+    return inicio->obtenerIzquierda();
 }
 
 template <typename T>
-Arbin<T>* Arbin<T>::arbol_derecho() const {
+Arbin<T>* Arbin<T>::arbolDerecho() const {
     assert (inicio);
-    return inicio->obtener_derecha();
+    return inicio->obtenerDerecha();
 }
 
 template <typename T>
-bool Arbin<T>::es_vacio()const {
+bool Arbin<T>::esVacio()const {
 	return(inicio==NULL);
 }
-
 template <typename T>
-bool Arbin<T>::es_hoja()const {
+bool Arbin<T>::esHoja()const {
     assert (inicio);
-    return ((inicio->obtener_izquierda()->inicio == NULL) &&
-            (inicio->obtener_derecha()->inicio == NULL));
+    return ((inicio->obtenerIzquierda()->inicio == NULL) &&
+            (inicio->obtenerDerecha()->inicio == NULL));
 }
-
-template <typename T>
-Arbin<T> & Arbin<T>::operator= (const Arbin<T> & a) {
-    vaciar(this);
-
-    if (a.inicio == NULL )
-        inicio = NULL;
-    else
-        inicio =  new NodoArbol (a.inicio->obtener_dato(),
-                                (a.inicio->obtener_izquierda()),
-                                (a.inicio->obtener_derecha()));
-
-    return *this;
-}
-
 
 template <typename T>
 int Arbin<T>::altura() const
 {
-   if (this->es_vacio()) return 0;
-   if (this->es_hoja()) return 1;
-   int alturaDer = inicio->obtener_derecha()->altura();
-   int alturaIzq = inicio->obtener_izquierda()->altura();
-   return (alturaDer > alturaIzq)? 1+ alturaDer : 1 + alturaIzq;
+    if (this->esVacio())
+        return 0;
+    if (this->esHoja())
+        return 1;
+    int alturaDer = inicio->obtenerDerecha()->altura();
+    int alturaIzq = inicio->obtenerIzquierda()->altura();
+    if (alturaDer > alturaIzq)
+        return 1+ alturaDer;
+    else
+        return 1 + alturaIzq;
 }
 
-
 template <typename T>
-bool Arbin<T>::existe_elemento(T& elem) const
+bool Arbin<T>::existeElemento(const T& elem) const
 {
-   if (this->es_vacio()) return false;
-   if (this->inicio->obtener_dato()== elem) return true;
+   if (this->esVacio())
+	   return false;
+   if (this->inicio->getDato()== elem)
+	   return true;
 
-   return inicio->obtener_derecha()-> existe_elemento(elem)||
-                   inicio->obtener_izquierda()->existe_elemento(elem);
+   return inicio->obtenerDerecha()->existeElemento(elem)||
+            inicio->obtenerIzquierda()->existeElemento(elem);
 }
 
 
 template <typename T>
 int Arbin<T>::cantidadNodos() const
-{   if (this->es_vacio()) return 0;
+{   if (this->esVacio())
+        return 0;
     return 1 +
-        this->inicio->obtener_izquierda()->cantidadNodos() +
-        this->inicio->obtener_derecha()->cantidadNodos();
+        this->inicio->obtenerIzquierda()->cantidadNodos() +
+        this->inicio->obtenerDerecha()->cantidadNodos();
 }
 
-template <typename T>
-int Arbin<T>::operator== (const Arbin<T>& otroArbol)const
-{ if (this->es_vacio() && otroArbol.es_vacio()) return true;
-  if (!this->es_vacio() &&  !otroArbol.es_vacio() )
-        return  (this->inicio->obtener_dato() == otroArbol.inicio->obtener_dato() ) &&
-           this->inicio->obtener_izquierda()-> operator== ( *otroArbol.inicio->obtener_izquierda() ) &&
-           this->inicio->obtener_derecha()-> operator== ( *otroArbol.inicio->obtener_derecha() );
 
+template <typename T>
+bool Arbin<T>::operator== (const Arbin<T>& otroArbol)const
+{
+    if (this->esVacio() && otroArbol.esVacio())
+        return true;
+    if (!this->esVacio() &&  !otroArbol.esVacio() )
+        return  (this->inicio->getDato() == otroArbol.inicio->getDato() ) &&
+           this->inicio->obtenerIzquierda()->operator== ( *otroArbol.inicio->obtenerIzquierda() ) &&
+           this->inicio->obtenerDerecha()->operator== ( *otroArbol.inicio->obtenerDerecha() );
    else
         return false;
 }
 
+template<typename T>
+Arbin<T> & Arbin<T>::operator= (const Arbin<T> & otro)
+{
+    if (otro.inicio == NULL)
+        inicio = NULL;
+    else
+        inicio = new NodoArbin(otro.inicio->getDato(),otro.inicio->obtenerIzquierda(),otro.inicio->obtenerDerecha());
+}
+
+template <typename T>
+void Arbin<T>::preOrden(std::ostream & salida) const {
+    if (!this->esVacio()) {
+         salida << inicio->getDato()<<std::endl ;
+         inicio->obtenerIzquierda()->preOrden(salida);
+         inicio->obtenerDerecha()->preOrden(salida);
+    }
+
+}
+
 template class Arbin<int>;
 template class Arbin<char>;
+template class Arbin<float>;
